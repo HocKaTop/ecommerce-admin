54b5d323ac6085d26ca602c67040542af6ad32ad
Slobodeniuc Oleg <oleg.slobodeniuc@iis.utm.md>
2025-09-18 16:22:58 +0300

    fixed billboards server component

--- app/(dashboard)/[storeID]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeID]/(routes)/billboards/page.tsx
@@ -24,13 +24,13 @@ const BillboardsPage = async ({
   const formatedBillboards: BillboardColumn[] = billboards.map((item)=>({
     id:item.id,
     label: item.label,
-    createdAT: format(item.createdAt, "MMMM do, yyyy")
+    createdAt: format(item.createdAt, "MMMM do, yyyy")
   }))
   
   return (
     <div className=' flex-col'>
         <div className='flex-1 space-y-4 p-8 pt-6'>
-        <BillboardClient data={billboards} />
+        <BillboardClient data={formatedBillboards} />
         </div>
     </div>
   )
